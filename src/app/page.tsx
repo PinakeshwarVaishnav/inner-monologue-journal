0ae6674b7f9537394ed3fcaf153039f0f5d39c3b
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2025-02-23 19:50:34 +0530

    Add gradient background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
-
 export default function Home() {
   return (
-      );
+    <div className="bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 flex justify-center min-h-screen text-gray-100">
+     Start typing your thoughts... 
+    </div>
+  );
 }
