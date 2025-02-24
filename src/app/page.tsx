63f29a6a3ec11ec723fa0c02365334ad1ceafd61
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2025-02-24 19:40:40 +0530

    feat: add textarea for journal entry input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <div className="bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 flex justify-center min-h-screen text-gray-100">
-     Start typing your thoughts... 
+    <div className="bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 flex min-h-screen w-screen text-gray-100">
+      <textarea
+        className="bg-transparent h-full w-full flex-1 border-none resize-none outline-none p-4 m-4 overflow-auto"
+        placeholder="Start typing your thoughts..."
+      />
     </div>
   );
 }
