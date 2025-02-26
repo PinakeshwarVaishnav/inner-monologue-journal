63b0bad75107019f5cbbb696ea9889238b260c48
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2025-02-26 19:58:31 +0530

    Feat: Expand textarea to fill page
    Modified CSS to allow the textarea to take up most of the page space, improving the user interface for writing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 flex min-h-screen w-screen text-gray-100">
       <textarea
-        className="bg-transparent h-full w-full flex-1 border-none resize-none outline-none p-4 m-4 overflow-auto"
+        className="bg-transparent h-[calc(100vh -16px)] w-full resize-none outline-none p-4 m-4 text-white text-xl font-mono"
         placeholder="Start typing your thoughts..."
       />
     </div>
