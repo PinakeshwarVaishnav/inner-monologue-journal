c23d1f649116d7e5e598a2fc6daf8d357a1300e0
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2025-02-27 19:32:40 +0530

    feat: add timer buttons for 5min, 10min, and 15min

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 flex min-h-screen w-screen text-gray-100">
-      <textarea
-        className="bg-transparent h-[calc(100vh -16px)] w-full resize-none outline-none p-4 m-4 text-white text-xl font-mono"
-        placeholder="Start typing your thoughts..."
-      />
+    <div className="bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 flex flex-col min-h-screen w-screen text-gray-100">
+      <header className="p-4 flex justify-center items-center space-x-4">
+        <Button variant="outline" className="bg-gray-700">
+          5 min
+        </Button>
+        <Button variant="outline" className="bg-gray-700">
+          10 min
+        </Button>
+        <Button variant="outline" className="bg-gray-700">
+          15 min
+        </Button>
+      </header>
+      <main>
+        <textarea
+          className="bg-transparent h-full  w-80 resize-none outline-none p-4 m-4 text-white text-xl"
+          placeholder="Start typing your thoughts..."
+        />
+      </main>
     </div>
   );
 }
